e85c95127bec2e52f9f8861923e6eb3e23e41a64
毛毛虫小姐 <13007107675@163.com>
2020-12-01 22:59:54 +0800

    refactor: test-1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# VUU3.0
+# VUE.0
+<!-- v3.vuejs.org -->
+
+新特性
+1：采用渐进式开发，向下兼容
+2：性能提升：
+    打包大小减少 41%
+    初次渲染快 55%
+    更新快 133%
+    内存使用减少54%
+3：composition API 集合，解决VUE2组件开发问题
+4：新特性
+    Teleport 瞬移组件
+    Suspense 异步加载组件
+5：使用 TypeScript
+
+构建步骤
+方法一：终端创建
+1: npm install -g @vue/cli
+2: vue create projectName
+3: 选择自定义的选项
+
+方法二：图形界面操作
+1: vue  ui
+2: 在图形界面进行选择
---
+++ test-1/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h2>欢迎光临点餐中心</h2>
+  <div>请选择一位服务员为您服务</div>
+  <div>
+    <button
+      v-for="(item, index) in waiter"
+      v-bind:key="index"
+      @click="selectWaiterfn(index)"
+    >
+      {{ `${index}:${item}`}}
+      </button>
+  </div>
+  <div>你选择了【{{ selectWaiter }}】服务员为你服务</div>
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const waiter = ref(['Alias', 'Jack', 'smith'])
+    const selectWaiter = ref('')
+    const selectWaiterfn = (index: number)=>{
+      selectWaiter.value = waiter.value[index]
+    }
+    return {
+      waiter,
+      selectWaiter,
+      selectWaiterfn
+    } 
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
